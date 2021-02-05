fa66f366504967367d981938572ffbdcdf884aab
Damion Junk <junkda@gmail.com>
2021-02-04 22:15:00 -0600

    Implementing CLI and "Step 1" requirements.

--- src/grdji/core.clj
+++ src/grdji/core.clj
@@ -1,8 +1,34 @@
 (ns grdji.core
   (:require [grdji.sys.core :as c]                          ;; system bootstrapping located in sys.core
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [grdji.file :as gfile]
+            [grdji.sys.config :refer [config]])
+  (:import (java.io StringWriter)))
+
+(defn handler
+  "Primary program flow/control logic.
+
+  Starts a REPL, does file IO, or starts a REST webserver."
+  []
+  (cond
+    (-> config :options :repl)
+    (do (log/info "Starting in REPL only mode."))
+
+    (-> config :options :file)
+    (do (log/info "File input mode, reading from:" (-> config :options :file))
+        (let [sort-option (-> config :options :option (#(str "option-" %)) keyword)]
+          (log/info "Sort mode: " sort-option)
+          (with-open [st (StringWriter.)]
+            (println "")
+            (gfile/->csv! (gfile/file->sorted-recs (-> config :options :file) sort-option) st)
+            (println (str st))))
+        (c/stop-app))))
 
 (defn -main
   [& args]
-  (log/info "DJI Console Starting")
-  (c/start-app args))
+  (let [{:keys [exit-message ok?]} (c/validate-args args)]
+    (when exit-message
+      (println exit-message)
+      (System/exit (if ok? 0 1)))
+    (log/info "DJI Console Starting")
+    (c/start-app args handler)))
--- src/grdji/sys/core.clj
+++ src/grdji/sys/core.clj
@@ -2,8 +2,10 @@
   (:require [clojure.tools.logging :as log]
             [clojure.tools.cli :refer [parse-opts]]
             [mount.core :as mount :refer [defstate]]
+            [clojure.java.io :as io]
             [grdji.sys.nrepl]
-            [grdji.sys.config]))
+            [grdji.sys.config :refer [config]]
+            [clojure.string :as s]))
 
 ;; log uncaught exceptions in threads
 (Thread/setDefaultUncaughtExceptionHandler
@@ -14,20 +16,64 @@
                   :where     (str "Uncaught exception on" (.getName thread))}))))
 
 (def cli-options
-  [["-c" "--config EDNFILE"  "EDN Config Overrides"]])
+  [["-c" "--config EDNFILE"  "EDN Config Overrides"]
+   ["-r" "--repl"            "Start a repl and do nothing."]
+   ["-f" "--file INPUT"      "Input Records File"
+    :parse-fn io/file
+    :validate [#(.exists %) "Input file not found."]]
+   ["-o" "--option OPT" "Sort option 1: email(desc)last(asc) 2: dob(asc) 3: last(desc)"
+    :parse-fn #(Long. %)
+    :validate [#(and (> % 0) (<= % 3)) "Sort option must be 1, 2, or 3."]
+    :default 1]
+   ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["grdji record parser"
+        ""
+        "Usage: grdji [options]"
+        ""
+        "Options:"
+        options-summary]
+       (s/join "\n")))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (s/join \n errors)))
+
+(defn validate-args
+  [args]
+  (let [{:keys [options arguments errors summary] :as opts} (parse-opts args cli-options)]
+    (cond
+      (:help options) ; help => exit OK with usage summary
+      {:exit-message (usage summary) :ok? true}
+
+      ;; Ignore commands for migration and just start the repl
+      (:repl options) (merge opts {:repl? true})
+
+      errors ; errors => exit with description of errors
+      {:exit-message (error-msg errors)}
+
+      ;; The only other valid runmode besides REPL for now.
+      (:file options) opts
+
+      :else ; failed custom validation => exit with usage summary
+      {:exit-message (usage summary)})))
 
 (defn stop-app []
   (doseq [component (:stopped (mount/stop))]
-    (log/info component "stopped"))
+    ;(log/info component "stopped")
+    )
   (shutdown-agents))
 
 (defn get-components [args]
   (-> args
-      (parse-opts cli-options)
+      validate-args
       mount/start-with-args
       :started))
 
-(defn start-app [args]
+(defn start-app [args handler]
   (doseq [component (get-components args)]
     (log/info component "started"))
+
+  (handler)
   (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app)))
